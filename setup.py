7903ebc8ac0eac4b43289fc23daaf70eaeaf020b
Matt Hamilton <matth@netsight.co.uk>
2013-03-18 12:45:03 +0000

    Removed paste dependancies in setup.py

--- setup.py
+++ setup.py
@@ -69,6 +69,4 @@ setup(name='netsight.windowsauthplugin',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
